8cbd2f91fc0ed3537228e5fb320c5fd2fe5231c6
ksfrankdou0423 <frankdou0423@126.com>
2017-03-24 17:03:13 +0800

    Issue #2   changed

---
+++ owepcommon/src/main/java/com/hd/pojo/Product.java
@@ -0,0 +1,28 @@
+package com.hd.pojo;
+
+/**
+ * Created by frankdou on 2017/3/24.
+ */
+public class Product {
+
+    private  int id;
+
+    private  String proName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProName() {
+        return proName;
+    }
+
+    public void setProName(String proName) {
+        this.proName = proName;
+    }
+}
+
